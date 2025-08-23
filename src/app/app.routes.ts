431c9fbe06b5cd2cd7b26fdc7dd35778e644660c
OuFFA-8 <ouffaalaa26@gmail.com>
2025-08-23 19:11:35 +0300

    xc

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,7 +10,7 @@ export const routes: Routes = [
         path: 'home',
         loadComponent: () =>
             import('./pages/home/home.component').then(m => m.HomeComponent),
-        title: 'Vsta | الصفحة الرئيسية'
+        title: 'Vsta | الرئيسية'
     },
     {
         path: 'portfolio/:id',
